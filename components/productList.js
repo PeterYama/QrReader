c31920f936ceb8b0b5f839e42577bdc98fdc5e53
PeterYama <35582343+PeterYama@users.noreply.github.com>
2020-01-28 22:39:56 +1100

    new product view

--- components/productList.js
+++ components/productList.js
@@ -1,34 +1,60 @@
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import React , { useState } from 'react';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
 
 export default function productList(props) {
+
     const { navigation } = props;
     const [products,setProducts] = useState('');
     var data = navigation.getParam('data');
+    populate(data);
+
+    const styles = StyleSheet.create({
+      container:{
+        flex: 1,
+        backgroundColor: '#DAF2FD',
+        paddingTop:40,
+        paddingHorizontal: 20
+      },
+      item:{
+        marginTop:24,
+        padding:30,
+        backgroundColor:'#CCD5FC',
+        fontSize:24,
+        marginHorizontal:10,
+        marginTop:24
+      }
+    })
     
-    function populate(data){
-        fetch(data).then(res => res.json())
+    return (
+      <View style={styles.container}>
+            <ScrollView>
+              { products === '' ? console.log('waiting') :  products.map((item) => {
+                return (
+                    <View style={styles.item}>
+                      <Text>{item.title}</Text>
+                      <Text>{item.type}</Text>
+                      <Text>{item.description}</Text>
+                      <Text>$ {item.price} AUD</Text>
+                    </View>
+                )
+              }) }
+          </ScrollView>
+      </View>
+    );
+
+    async function populate(data){
+      await fetch(data).then( res =>  res.json())
         .then(
           (result) => {
             // console.log("result: "+ JSON.stringify(result))
             setProducts(result);
-            console.log(products);
           },
           (error) => {
             console.log(`Error while calling the API : ${error}`)
           }
     );  
     }
+
     
-    populate(data);
     
-    return (
-      <View>
-       <Text>Product List</Text>
-       <Text>data = {JSON.stringify(navigation.getParam('data'))}</Text>
-       <Text>type = {JSON.stringify(navigation.getParam('type'))}</Text>
-       <Text>{JSON.stringify(products)}</Text>
-      </View>
-      
-    );
 }
